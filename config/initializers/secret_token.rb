510866abfe0cfe501557fe60d9705220dfd0e004
tochman <thomas@nocebo.se>
2013-08-29 23:38:34 -0700

    Initial commit Spree application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OhSpree::Application.config.secret_key_base = '03b13bc0e7d32df8636ade7e4f7e41a4bbe3670b0fbb0ac7c71480504cee5821e80650b28d7c87217996e526b5b0f7c41fdaf1cb047e21d05713a364198382b3'
